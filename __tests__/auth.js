990d3ae85c2e322270cc3cd7b0bbac11ac6f2d02
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-11-13 11:24:10 -0600

    added register route and test passes for it

--- __tests__/auth.js
+++ __tests__/auth.js
@@ -1,6 +1,25 @@
-const request = require('supertest');
-const server = require('../api/server');
+const request = require("supertest");
+const server = require("../api/server");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
+beforeEach(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+  await db.seed.run;
+});
 afterAll(async () => {
   await db.destroy();
-})
+});
+
+describe("testing auth integration", () => {
+  it("registers a user", async () => {
+    const newUser = { username: "testing", password: "testing" }
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send(newUser);
+    expect(res.status).toBe(201);
+    expect(res.type).toBe("application/json");
+    expect(res.body.username).toBe("testing");
+  });
+});
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,12 +1,28 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./users-model");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", async (req, res, next) => {
+  const {username, password} = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  try {
+    const user = await Users.findByUsername(username);
+    if(user) {
+      res.status(409).json({message: 'username already exists'});
+    }
+    
+    const newUser = await Users.add({
+      username,
+      password: hash,
+    })
+    res.status(201).json(newUser);
+  } catch(err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
